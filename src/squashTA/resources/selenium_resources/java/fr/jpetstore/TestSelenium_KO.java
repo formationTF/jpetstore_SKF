a1766f5c924cebd69afa0f57f61d7bfab3846347
RecetteSquash <qdusser@qualixo.com>
2018-12-20 18:05:02 +0100

    options firefox import

--- src/squashTA/resources/selenium_resources/java/fr/jpetstore/TestSelenium_KO.java
+++ src/squashTA/resources/selenium_resources/java/fr/jpetstore/TestSelenium_KO.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class TestSelenium_KO {
 
